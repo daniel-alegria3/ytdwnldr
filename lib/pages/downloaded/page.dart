5164c3c7bb2cbe3d0ff6e160f4b2462ff7fe2176
Daniel A. Sallo <215270@unsaac.edu.pe>
2026-01-03 19:05:12 -0500

    Add `BottomNavigationBar` and skeleton for its pages

---
+++ lib/pages/downloaded/page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DownloadedPage extends StatelessWidget {
+  const DownloadedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Pagina Downloaded");
+  }
+}
